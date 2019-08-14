cbfd4d56f20c8c2e8e2e0e1912e57181cb136809
patrickclery <patrick.clery@gmail.com>
2019-08-13 19:19:08 -0700

    :white_check_mark: Adding tests.

---
+++ spec/main_spec.rb
@@ -0,0 +1,32 @@
+RSpec.describe 'Main.rb' do
+  it 'recursively reads all of the images from the supplied directory of images' do
+    pending
+  end
+  it 'extracts their EXIF GPS data (longitude and latitude)' do
+    pending
+  end
+  it 'writes the name of that image' do
+    pending
+  end
+  it 'GPS co-ordinates it finds to a CSV file' do
+    pending
+  end
+  it 'is executable from the command-line' do
+    pending
+  end
+  it 'scans the current working directory by default' do
+    pending
+  end
+  it 'accepts a directory as an optional parameter' do
+    pending
+  end
+
+  context 'output in different formats' do
+    it 'can output to CSV' do
+      pending
+    end
+    it 'can output to HTML' do
+      pending
+    end
+  end
+end
