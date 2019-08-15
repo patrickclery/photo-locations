8c7877c7ae84abdcf36540b999242ed342c69c2b
patrickclery <patrick.clery@gmail.com>
2019-08-14 21:18:20 -0700

    Had to move this back. Took me a bit, but it needs ".parse!(into: ...)" to work so I passed in an empty hash and succes

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe 'Shell application' do
     expect(File.executable?("./application.rb")).to be true
   end
 
+  it 'can accept an optional output file' do
+
+  end
+
+  it 'can create a CSV file' do
+    exec("./application.rb -o #{tmp_file.path}")
+    expect(exit_status).to be true
+
+    csv = File.open(tmp_file.path)
+    expect(csv.size).to be_nonzero
+  end
+
 end
