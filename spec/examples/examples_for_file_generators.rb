68bacd42ffbe294defa2d53b42bf4a1224305642
patrickclery <patrick.clery@gmail.com>
2019-08-14 22:44:57 -0700

    stubbed out Dir.pwd so it returns a fake dir, now we test that it acts on Dir.pwd

--- spec/examples/examples_for_file_generators.rb
+++ spec/examples/examples_for_file_generators.rb
@@ -2,14 +2,16 @@ RSpec.shared_examples 'examples_for_file_generators' do
 
   it { should respond_to(:call).with_keywords(:target_directory, :output_file) }
 
+  before do
+    allow(Dir).to receive(:pwd).and_return('/fake/dir')
+  end
+
   it 'scans the current working directory by default' do
     expect(Dir).to receive(:glob)
                      .with("#{Dir.pwd}/**/*.jpg")
 
-    subject.call(output_filename: tmp_file.path)
+    subject.call(output_file:      tmp_file.path,
+                 target_directory: '/fake/dir')
   end
 
-  it 'accepts a directory as an optional parameter' do
-    expect(subject).to respond_to(:call).with_keywords(:path)
-  end
 end
--- spec/services/generate_csv_spec.rb
+++ spec/services/generate_csv_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe Lendesk::GenerateCSV do
 
   include_context 'stub_filenames_and_images'
+  include_examples 'examples_for_file_generators'
 
   it 'should create a new CSV file' do
     subject.call(target_directory: '/fake/dir', output_file: tmp_file)
